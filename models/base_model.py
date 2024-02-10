e16a807ff9650e3071b07f6e77cd13ec4a350e2a
merlin254-X <merlinderick8@gmail>
2024-02-10 08:54:56 -0800

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+class BaseModel that defines all common attributes/methods for other classes
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """
+        Assign a unique ID using uuid.uuid4()
+        """
+        self.id = str(uuid.uuid4())
+        """
+        Assign the current datetime to created_at and updated_at
+        """
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        setattr(self, key, value)
+
+    def save(self):
+            """
+            Update the updated_at attribute with the current datetime
+            """
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+            """
+            Custom string representation
+            """
+            class_name = self.__class__.__name__
+            return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        
+    def to_dict(self):
+                """
+                Create a dictionary representation of the instance
+                """
+                instance_dict = self.__dict__.copy()
+
+                """
+                Add the __class__ key with the class name
+                """
+                instance_dict["__class__"] = self.__class__.__name__
+
+                """
+                Convert created_at and updated_at to ISO format strings              """
+
+                instance_dict["created_at"] = self.created_at.isoformat()
+                instance_dict["updated_at"] = self.updated_at.isoformat()
+                return instance_dict
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key, value in my_model_json.items():
+        print(f"\t{key}: ({type(value)}) - {value}")
+
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+
+"""
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        """
+        """
+        my_model = BaseModel()
+        self.assertIsInstance(my_model.id, str)
+        self.assertIsInstance(my_model.created_at, datetime)
+        self.assertIsInstance(my_model.updated_at, datetime)
+        self.assertEqual(my_model.created_at, my_model.updated_at)
+
+    def test_save_method(self):
+        """
+        Test if the save method updates the updated_at attribute
+        """
+        my_model = BaseModel()
+        old_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(old_updated_at, my_model.updated_at)
+
+    def test_str_representation(self):
+        """
+        Test if the __str__ method produces the expected output
+        """
+        my_model = BaseModel()
+        expected_output = f"[{my_model.__class__.__name__}] ({my_model.id}) {my_model.__dict__}"
+        self.assertEqual(str(my_model), expected_output)
+
+    def test_to_dict_method(self):
+        """
+        Test if the to_dict method returns the expected dictionary
+        """
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+    
+    
+        self.assertIsInstance(my_model_dict, dict)
+    
+        
+        self.assertEqual(my_model_dict["__class__"], "BaseModel")
+        self.assertEqual(my_model_dict["id"], my_model.id)
+        self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
+
+if __name__ == "__main__":
+    unittest.main()
